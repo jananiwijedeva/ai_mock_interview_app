f61bdf0f4231cf2c289f6a8d6d5ca355ae53702d
jananiwijedeva <janani.wijayantha3@gmail.com>
2025-04-25 00:20:15 +0530

    setup with shadcv ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-blue-500">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
